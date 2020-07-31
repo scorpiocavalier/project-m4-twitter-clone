adf8a7a91966e7f984facc219f747eee803dd5fa
Arturo Chen <art.can.fly@gmail.com>
2020-07-31 15:49:42 -0400

    add try catch block w/ async await on custom hook

--- client/src/hooks/useLoadCurrentUser.jsx
+++ client/src/hooks/useLoadCurrentUser.jsx
@@ -1,13 +1,17 @@
 import { useEffect } from 'react'
 import { STATUS, ACTIONS } from '../constants'
 
+const fetchData = async (dispatch) => {
+  try {
+    const res = await fetch('/api/me/profile')
+    const data = await res.json()
+    dispatch({ type: ACTIONS.SET_USER, payload: { user: data.profile } })
+    dispatch({ type: ACTIONS.SET_STATUS, payload: { status: STATUS.IDLE } })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 export const useLoadCurrentUser = (dispatch) => {
-  useEffect(() => {
-    fetch('/api/me/profile')
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: ACTIONS.SET_USER, payload: { user: data.profile } })
-        dispatch({ type: ACTIONS.SET_STATUS, payload: { status: STATUS.IDLE } })
-      })
-  }, [])
+  useEffect(() => { fetchData(dispatch) }, [])
 }
