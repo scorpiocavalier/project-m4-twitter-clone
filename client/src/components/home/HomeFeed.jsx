9aea26cf6552727ff992abbf69063c13d72aac70
Arturo Chen <art.can.fly@gmail.com>
2020-08-04 04:55:48 -0400

    create HomeFeed

--- client/src/components/home/HomeFeed.jsx
+++ client/src/components/home/HomeFeed.jsx
@@ -1,5 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+import Tweet from '../tweet/Tweet'
 
 export default () => {
-  return <div>Home Feed</div>
+  const [feed, setFeed] = useState({})
+
+  useEffect(() => {
+    const fetchHomeFeed = async () => {
+      const res = await fetch('/api/me/home-feed')
+      const { tweetsById } = await res.json()
+      setFeed({...tweetsById})
+    }
+    fetchHomeFeed()
+  }, [])
+
+  return (
+    <TweetFeedWrapper>
+      { feed &&
+        Object.values(feed).map((tweet, index) => {
+          return <Tweet key={ index } tweet={ tweet } />
+        })
+      }
+    </TweetFeedWrapper>
+  )
 }
+
+const TweetFeedWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
